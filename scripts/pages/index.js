d0afbb0814a4222f30c225ff41451d4ed2e634cc
Aurelien Allenic <aurelien.allenic@gmail.com>
2023-03-28 17:31:48 +0200

    (fix) corrected bug

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -34,7 +34,7 @@ for(let i= 0; i < photographers.photographers.length; i++){
         });
     };
 
-    async function init(e) {
+    async function init() {
       
         // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
